246b48c0af49268d8b251ebd6125adecac63ddba
Johora99 <fatimatuzjohora1999@gmail.com>
2025-05-13 21:43:54 +0600

    testimonial section added

--- src/components/Testimonial/TestimonialSlider.jsx
+++ src/components/Testimonial/TestimonialSlider.jsx
@@ -1,14 +1,19 @@
 'use client'
 
 import Image from "next/image";
-import Slider from "../Slider/Slider";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import icon from '../../../public/Vector.png'
+import './testimonial.css'
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 export default function TestimonialSlider({testimonial}) {
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true, // Ensure arrows are enabled
     responsive: [
@@ -34,17 +39,33 @@ export default function TestimonialSlider({testimonial}) {
   };
   return (
     <div className="slider-container pb-20">
-      <div className="w-full">
+      <div className="w-full testimonial-slider">
         <Slider {...settings}>
-       {
-        testimonial.map(category => <div className="px-5 "> 
-            <div className='cardBorder group text-center'>
-            <Image  src={testimonial.image} alt="" className="mx-auto"></Image>
-              <p className="text-base font-medium text-darkGray my-2 group-hover:text-darkGreen2">{category.name}</p>
-              <p className="text-sm font-normal text-lightGray">{category.totalProduct} Products</p>
+         {
+          testimonial.length > 0 ? testimonial.map(item => <div className="bg-white p-5 rounded-lg h-full">
+            <div className="flex flex-col justify-between grow h-full">
+              <Image src={icon} alt="" width={50} ></Image>
+              <p className="my-5 text-darkGray2 text-sm font-normal">{item?.quote}</p>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                <Image src={item?.user?.image} width={50} height={50} alt="" className="w-10 h-10 rounded-full object-cover"></Image>
+                <div>
+                  <p className="text-darkGray text-base font-medium">{item?.user?.name}</p>
+                  <p className="text-dimGray text-sm font-normal">{item?.user?.role}</p>
+                </div>
+                </div>
+                <div>
+                <Rating
+                style={{ maxWidth: 80 }}
+                 value={testimonial?.rating}
+                 readOnly
+                 />
+  
+                 </div>
+              </div>
             </div>
-          </div> )
-       }
+          </div>):<p>No testimonial is available</p>
+         }
           
         </Slider>
       </div>
