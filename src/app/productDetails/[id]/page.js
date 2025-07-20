f66b4a957b77eecaa2380516125d24a3fdd8967d
Johora99 <fatimatuzjohora1999@gmail.com>
2025-07-20 22:27:19 +0600

    update

--- src/app/productDetails/[id]/page.js
+++ src/app/productDetails/[id]/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default async function page({params}) {
   const id = await params.id;
   try{
-    const res = await axios.get(`http://localhost:3000/api/products/${id}`)
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/products/${id}`)
     const product = res.data || [];
     return (
       <div>{id}</div>
